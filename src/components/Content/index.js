7c68cae0fb385e69923da0217ea6918a263c0751
over2000 <danielvitorcg@gmail.com>
2022-11-17 11:49:04 -0300

    feat: list tracklist

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
 
 import { connect } from 'react-redux'
 
-const Content = ({ activeModule, activeList }) => (
+const Content = ({ activeList }) => (
   <Container maxWidth='md'>
     <Box>
-      <Typography variant='h5'>{activeModule.title}</Typography>
-      <Typography variant='h6'>{activeList.title}</Typography>
-      <Typography>{activeList.tracklist}</Typography>
-      {/* <List>
-        {activeLesson.tracklist.map((list) => (
+      <Typography variant='h5'>{activeList.title}</Typography>
+      <Typography>{Object.keys(activeList)[2]}</Typography>
+      <List>
+        {activeList?.tracklist?.map((list) => (
           <ListItem key={list}>
             <ListItemText primary={list} />
           </ListItem>
         ))}
-      </List> */}
+      </List>
     </Box>
   </Container>
 )
